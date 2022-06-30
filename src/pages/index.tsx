3328767cf31da56fd1c2d863640efe20317f3d6f
FernandoSegregio <segregio@gmail.com>
2022-06-30 01:49:01 -0300

    feat: organize project scope inside src folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <link href="https://img.wine.com.br/fenix/image/_big_bang/icons/favicon/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+      </Head>
+
+      <header>
+        header
+      </header>
+
+      <main>
+        main
+      </main>
+
+      <footer>
+        footer
+      </footer>
+    </>
+  );
+}
