c3274bcbab162605fa89255a4f276fd7933f61dc
FernandoSegregio <segregio@gmail.com>
2022-06-30 02:55:14 -0300

    style: add GlobalStyle in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
+import GlobalStyle from '../styles/GlobalStyle';
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Head>
         <link href="https://img.wine.com.br/fenix/image/_big_bang/icons/favicon/favicon.ico" rel="shortcut icon" type="image/x-icon" />
       </Head>
-
+      <GlobalStyle />
       <Header />
 
       <main>
