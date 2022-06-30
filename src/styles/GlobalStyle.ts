64c2f4d7b72d69a0a293aa7a868d728f3fa4ae69
FernandoSegregio <segregio@gmail.com>
2022-06-30 11:23:31 -0300

    style: add root font size Global

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,6 +3,21 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
 
   :root {
+
+    --font-size-10: 0.625rem; /* 10px */
+    --font-size-11: 0.6875rem; /* 11px */
+    --font-size-12: 0.75rem; /* 12px */
+    --font-size-14: 0.875rem; /* 14px */
+    --font-size-16: 1rem; /* 16px */
+    --font-size-18: 1.125rem; /* 18px */
+    --font-size-20: 1.25rem; /* 20px */
+    --font-size-23: 1.4375rem; /* 23px */ 
+    --font-size-24: 1.5rem; /* 24px */ 
+    --font-size-28: 1.75rem; /* 28px */ 
+    --font-size-32: 2rem; /* 32px */
+
+
+
     --brand-color-white-100: #FFFFFF;
     --brand-color-white-200: #F5F8FA;
 
