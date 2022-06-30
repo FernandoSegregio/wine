d050a96843178489c1cc8397bb5ae27b83074a79
FernandoSegregio <segregio@gmail.com>
2022-06-30 17:25:34 -0300

    feat: create _document, change link index pages for _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import Head from 'next/head';
 import Header from '../components/Header';
 import GlobalStyle from '../styles/GlobalStyle';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Saira:wght@300;400;500;700&display=swap" rel="stylesheet" />
-      </Head>
       <GlobalStyle />
       <Header />
-
       <main>
         main
       </main>
