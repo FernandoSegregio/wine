f6c6222e6d70278ead7e5edd8edc8cd2a01ace42
FernandoSegregio <segregio@gmail.com>
2022-06-30 15:58:51 -0300

    feat: render logo wine header, and cong width and heigth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import ItemsNav from '../../helpers/ItemsNav'
+import Image from 'next/image';
+import ItemsNav from '../../helpers/ItemsNav';
 import {
   HeaderContainer, Nav, Ul, Li,
 } from './style'
+import logoWine from '../../../public/images/header/logo-wine.svg';
 
 function Header() {
   return (
     <HeaderContainer>
+      <Image
+        src={logoWine}
+        alt="Logo Wine"
+        width={100}
+        height={28}
+      />
       <Nav>
         <Ul>
           { ItemsNav.map((item) => (
