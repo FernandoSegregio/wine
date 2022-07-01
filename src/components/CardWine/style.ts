7cabccfc69352ce37f246d2c3e510c1dbea8729a
FernandoSegregio <segregio@gmail.com>
2022-06-30 22:03:03 -0300

    style: create CardContainer, add box-shadow, height and width

---
+++ src/components/CardWine/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Image from 'next/image'
+
+export const CardContainer = styled.div`
+  box-shadow: 0px 9.73384px 14.6008px rgba(0, 0, 0, 0.1);
+  height: 388.36px;
+  width: 256px;
+`;
