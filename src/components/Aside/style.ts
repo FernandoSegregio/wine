85c84d85a90e850ecc7e3865687caa940b50174b
FernandoSegregio <segregio@gmail.com>
2022-07-01 17:02:26 -0300

    style: add style h1

--- src/components/Aside/style.ts
+++ src/components/Aside/style.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 const AsideContainer = styled.aside`
+  h1 {
+    color: var(--brand-color-black-200);
+    font-family: 'Neo Sans';
+    font-size: var(-font-size-20);
+    font-weight: var(--font-weight-bold);
+    line-height: 24px;
+  }
 `;
 
 export default AsideContainer;
