26d5566bd4f5ba0a48a03bbe555520553bb6dba3
FernandoSegregio <segregio@gmail.com>
2022-07-01 16:27:25 -0300

    style: center and position items on the card

--- src/components/CardWine/style.ts
+++ src/components/CardWine/style.ts
@@ -2,16 +2,21 @@ import styled from 'styled-components';
 import Image from 'next/image'
 
 export const ImageStyle = styled(Image)`
-  height: 198.57px;
-  min-width: 0%;
-  max-width: 0%;
-  min-height: 0%;
-  max-height: 0%;
+  margin: 0 auto;
+  max-width: 0;
+  min-height: 0;
+  max-height: 0;
 `;
 
 export const CardContainer = styled.div`
-  box-shadow: 0px 9.73384px 14.6008px rgba(0, 0, 0, 0.1);
-  height: 333px;
+  flex-direction: column;
+  display: flex;
+  height: 388.36px;
+  row-gap: 16px;
+  position: relative;
+  
+      box-shadow: 0px 9.73384px 14.6008px rgba(0, 0, 0, 0.1);
+      height: 333px;
   width: 256px;
 `;
 
@@ -22,20 +27,28 @@ export const Name = styled.h1`
   font-weight: var(--font-weight-bold);
   letter-spacing: -0.222488px;
   line-height: 19px;
+  margin: 0 auto;
   text-align: center;
   width: 232.64px;
+  height: 42.83px;
 `;
+
 export const DiscountContainer = styled.div`
   align-items: center; 
   display: flex;
+  column-gap: 5.35px;
+  justify-content: center;
 `;
 
 export const ValueDiscount = styled.p`
+  color: var(--brand-color-gray-100);
   font-family: 'Lato';
-  font-weight: var(--font-weight-bold);
   font-size: var(--font-size-11);
+  font-weight: var(--font-weight-bold);
   line-height: 16px;
   text-align: center;
   text-decoration-line: line-through;
+`;
+
   color: var(--brand-color-gray-100);
 `;
