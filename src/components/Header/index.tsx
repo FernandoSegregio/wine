5d0d765a53573f4287401167ab6172ea4992dabd
FernandoSegregio <segregio@gmail.com>
2022-06-30 21:16:15 -0300

    feat: render div IconsHeader, LogoAndNav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image';
 import ItemsNav from '../../helpers/ItemsNav';
 import {
-  HeaderContainer, Nav, Ul, Li,
+  HeaderContainer, Nav, Ul, Li, IconsHeader, LogoAndNav,
 } from './style'
 import logoWine from '../../../public/images/header/logo-wine.svg';
 import searchIcon from '../../../public/images/header/search.svg';
@@ -12,37 +12,41 @@ import cartIcon from '../../../public/images/header/cart.svg';
 function Header() {
   return (
     <HeaderContainer>
-      <Image
-        src={logoWine}
-        alt="Logo Wine"
-        width={100}
-        height={28}
-      />
-      <Nav>
-        <Ul>
-          { ItemsNav.map((item) => (
-            <Li key={item}>{item}</Li>
-          ))}
-        </Ul>
-      </Nav>
-      <Image
-        src={searchIcon}
-        alt="Search icon"
-        width={56}
-        height={56}
-      />
-      <Image
-        src={countIcon}
-        alt="Count Icon"
-        width={56}
-        height={56.15}
-      />
-      <Image
-        src={cartIcon}
-        alt="Cart Icon"
-        width={56}
-        height={56}
-      />
+      <LogoAndNav>
+        <Image
+          src={logoWine}
+          alt="Logo Wine"
+          width={100}
+          height={28}
+        />
+        <Nav>
+          <Ul>
+            { ItemsNav.map((item) => (
+              <Li key={item}>{item}</Li>
+            ))}
+          </Ul>
+        </Nav>
+      </LogoAndNav>
+      <IconsHeader>
+        <Image
+          src={searchIcon}
+          alt="Search icon"
+          width={56}
+          height={56}
+        />
+        <Image
+          src={countIcon}
+          alt="Count Icon"
+          width={56}
+          height={56.15}
+        />
+        <Image
+          src={cartIcon}
+          alt="Cart Icon"
+          width={56}
+          height={56}
+        />
+      </IconsHeader>
     </HeaderContainer>
   )
 }
