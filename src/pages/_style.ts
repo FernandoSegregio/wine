9cd6c227a01a20b15363bd80eeb22d88ab5b5ce4
FernandoSegregio <segregio@gmail.com>
2022-07-01 18:16:30 -0300

    style: add styles in span and h1

--- src/pages/_style.ts
+++ src/pages/_style.ts
@@ -4,7 +4,25 @@ const Container = styled.div`
   column-gap: 32px;
   display: grid;
   grid-template-columns: 256px 1fr;
-  padding: 31.77px 160px;
+  padding: 38.77px 160px;
+
+    & main > h1 > span {
+    color: var(--brand-color-gray-350);
+    font-family: Lato;
+    font-size: var(--font-size-18);
+    font-weight: var(--font-weight-bold);
+    line-height: 22px;
+    padding-bottom: 32px;
+    }
+
+    & main > h1 {
+    color: var(--brand-color-gray-350);
+    font-family: Lato;
+    font-size: var(--font-size-18);
+    font-weight: var(--font-weight-normal);
+    line-height: 22px;
+    padding-bottom: 32px;
+    }
 `;
 
 export default Container;
