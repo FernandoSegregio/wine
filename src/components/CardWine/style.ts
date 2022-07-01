4a8f486f6c0d11205553eda4fb20ae02787fc9eb
FernandoSegregio <segregio@gmail.com>
2022-06-30 23:18:31 -0300

    style: add component Name, and create style

--- src/components/CardWine/style.ts
+++ src/components/CardWine/style.ts
@@ -11,6 +11,17 @@ export const ImageStyle = styled(Image)`
 
 export const CardContainer = styled.div`
   box-shadow: 0px 9.73384px 14.6008px rgba(0, 0, 0, 0.1);
-  height: 388.36px;
+  height: 333px;
   width: 256px;
+`;
+
+export const Name = styled.h1`
+  color: var(--brand-color-gray-400);
+  font-family: 'Lato';
+  font-size: var(--font-size-16);
+  font-weight: var(--font-weight-bold);
+  letter-spacing: -0.222488px;
+  line-height: 19px;
+  text-align: center;
+  width: 232.64px;
 `;
