63033e54e8d332260181b39b70813164378610ff
FernandoSegregio <segregio@gmail.com>
2022-07-01 16:57:00 -0300

    style: create component Container for main page

---
+++ src/pages/_style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  column-gap: 32px;
+  display: grid;
+  grid-template-columns: 256px 1fr;
+  padding: 31.77px 160px;
+`;
+
+export default Container;
