2ccbdac487ec4da36d1b22ce1231c4e6451ab922
FernandoSegregio <segregio@gmail.com>
2022-07-01 18:44:21 -0300

    style: add style in p and label inputs radios

--- src/components/Aside/style.ts
+++ src/components/Aside/style.ts
@@ -3,10 +3,27 @@ import styled from 'styled-components';
 const AsideContainer = styled.aside`
   h1 {
     color: var(--brand-color-black-200);
-    font-family: 'Neo Sans';
+    font-family: Lato;
     font-size: var(-font-size-20);
     font-weight: var(--font-weight-bold);
     line-height: 24px;
+  }
+
+  p {
+    color: var(--brand-color-gray-300);
+    font-family: Lato;
+    font-size: var(-font-size-18);
+    font-weight: var(--font-weight-bold);
+    line-height: 22px
+  }
+
+  label {
+    display: flex;
+    color: var(--brand-color-gray-300);
+    font-family: Lato;
+    font-size: var(-font-size-14);
+    font-weight: var(--font-weight-normal);
+    line-height: 17px
   }
 `;
 
