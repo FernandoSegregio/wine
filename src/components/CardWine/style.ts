be9d7d45dca35dfc80b99c3bcc1af7246b6afa6f
FernandoSegregio <segregio@gmail.com>
2022-06-30 22:10:39 -0300

    style: create ImageStyle and defines sizes

--- src/components/CardWine/style.ts
+++ src/components/CardWine/style.ts
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 import Image from 'next/image'
 
+export const ImageStyle = styled(Image)`
+  height: 198.57px;
+  min-width: 0%;
+  max-width: 0%;
+  min-height: 0%;
+  max-height: 0%;
+`;
+
 export const CardContainer = styled.div`
   box-shadow: 0px 9.73384px 14.6008px rgba(0, 0, 0, 0.1);
   height: 388.36px;
