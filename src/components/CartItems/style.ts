45a1dc37e11d84481444f59230c6ddcd43dedb8c
FernandoSegregio <segregio@gmail.com>
2022-07-02 00:58:08 -0300

    style: add styles in button and alphabetize items

--- src/components/CartItems/style.ts
+++ src/components/CartItems/style.ts
@@ -1,43 +1,51 @@
 import styled from 'styled-components';
 
 export const CartContainer = styled.div`
-  right: 0;
-  z-index: 2;
+  -webkit-transition-delay: 0s;
+  -webkit-transition-duration: 0.5s;
+  -webkit-transition-property: all;
+  -webkit-transition-timing-function: ease;
   background-color: var(--brand-color-white-150);
   height: 100vw;
   position: fixed;
-  transition-property: all;
-  -webkit-transition-property: all;
-  transition-duration: 0.5s;
-  -webkit-transition-duration: 0.5s;
-  transition-timing-function: ease;
-  -webkit-transition-timing-function: ease;
+  right: 0;
   transition-delay: 0s;
-  -webkit-transition-delay: 0s;
+  transition-duration: 0.5s;
+  transition-property: all;
+  transition-timing-function: ease;
   width: 350px;
+  z-index: 2;
 `;
 
 export const HeaderCart = styled.header`
-  height: 60px;
   background-color: var(--brand-color-white-100);
   display: flex;
+  height: 60px;
   padding: 20px;
 
-    > svg {
-      font-size: 22px;
+    > button {
+      background-color: transparent;
+      border: none;
+      height: 20px;
       margin-right: 20px;
+      width: 22px;
+      
+        > svg {
+          font-size: 22px;
+        }
     }
 
+
     > p {
-    font-family: sans-serif;
     color: var(---brand-color-black-200);
+    font-family: sans-serif;
     font-size: var(--font-size-20);
     font-weight: var(--font-weight-normal);
     } 
 
     > span {
-    font-family: sans-serif;
     color: var(---brand-color-black-200);
+    font-family: sans-serif;
     font-size: var(--font-size-20);
     font-weight: var(--font-weight-normal);
     } 
