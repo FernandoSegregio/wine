8868702166cbf6b053f0fdae6933276c44a0f913
FernandoSegregio <segregio@gmail.com>
2022-07-02 14:08:00 -0300

    feat: add div BorderInput

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,31 +1,48 @@
 import React from 'react';
-import AsideContainer from './style';
+import { AsideContainer, BorderInput } from './style';
 
 function Aside() {
   return (
     <AsideContainer>
       <h1>Refine sua busca</h1>
       <p>Por preço</p>
-      <label htmlFor="price-one">
+
+      <BorderInput htmlFor="price-one">
+        <span>Até R$40</span>
+        <label htmlFor="price-one" />
         <input type="radio" value="1" name="price" id="price-one" />
-        Até R$40,00
-      </label>
-      <label htmlFor="price-two">
+      </BorderInput>
+
+      <BorderInput htmlFor="price-two">
+        <span>R$40 a R$60</span>
+        <label htmlFor="price-two" />
         <input type="radio" value="2" name="price" id="price-two" />
-        R$40,00 a R$100,00
-      </label>
-      <label htmlFor="price-three">
+      </BorderInput>
+
+      <BorderInput htmlFor="price-three">
+        <span>R$60 a R$100</span>
+        <label htmlFor="price-three" />
         <input type="radio" value="3" name="price" id="price-three" />
-        R$100,00 a R$200,00
-      </label>
-      <label htmlFor="price-four">
+      </BorderInput>
+
+      <BorderInput htmlFor="price-four">
+        <span>R$100 a R$200</span>
+        <label htmlFor="price-four" />
         <input type="radio" value="4" name="price" id="price-four" />
-        R$200,00 a R$500,00
-      </label>
-      <label htmlFor="price-five">
+      </BorderInput>
+
+      <BorderInput htmlFor="price-five">
+        <span>R$200 a R$500</span>
+        <label htmlFor="price-five" />
         <input type="radio" value="5" name="price" id="price-five" />
-        Acima de R$500,00
-      </label>
+      </BorderInput>
+
+      <BorderInput htmlFor="price-six">
+        <span>Acima De R$500</span>
+        <label htmlFor="price-six" />
+        <input type="radio" value="6" name="price" id="price-six" />
+      </BorderInput>
+
     </AsideContainer>
   )
 }
