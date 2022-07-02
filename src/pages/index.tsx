37570c45441fe300d90d6c80dd036efd35ab3e47
FernandoSegregio <segregio@gmail.com>
2022-07-02 00:51:48 -0300

    feat: create page, route / and redirect for route /loja-vinhos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,11 @@
-import axios from 'axios';
-import { GetStaticProps } from 'next';
-import React from 'react';
-import Aside from '../components/Aside';
-import Grid from '../components/GridWine';
-import Header from '../components/Header';
-import { HomeProps } from '../interfaces';
-import GlobalStyle from '../styles/GlobalStyle';
-import Container from './_style';
+import { useEffect } from 'react';
+import Router from 'next/router';
 
-export default function Home({ wines, totalProduct }: HomeProps) {
-  return (
-    <>
-      <GlobalStyle />
-      <Header />
-      <Container>
-        <Aside />
-        <main>
-          <h1>
-            <span>{totalProduct}</span>
-            {' '}
-            produtos encontrados
-          </h1>
-          <Grid wines={wines} />
-        </main>
-      </Container>
-      <footer>
-        footer
-      </footer>
-    </>
-  );
+export default function Comp() {
+  useEffect(() => {
+    const { pathname } = Router;
+    if (pathname === '/') {
+      Router.push('/loja-vinhos');
+    }
+  });
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await axios.get('https://wine-back-test.herokuapp.com/products?page=1&limit=9');
-  console.log(data.totalItems);
-
-  return {
-    props: {
-      wines: data.items,
-      totalProduct: data.totalItems,
-    },
-    revalidate: 60 * 60 * 24,
-  };
-};
