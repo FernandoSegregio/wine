0566ddadd5f1a60ccbdded760905622ba6d29e80
FernandoSegregio <segregio@gmail.com>
2022-07-02 00:50:29 -0300

    feat: create items HeaderCart and logic close Cart

--- src/components/CartItems/index.tsx
+++ src/components/CartItems/index.tsx
@@ -4,7 +4,14 @@ import { CartContainer, HeaderCart } from './style';
 
 function Cart({ open, setOpen }) {
   return (
-    <CartContainer hidden={open}>
+    <CartContainer hidden={!open}>
+      <HeaderCart>
+        <button type="button" onClick={() => setOpen(false)}>
+          <BsArrowLeft />
+        </button>
+        <p>WineBox</p>
+        <span>(0)</span>
+      </HeaderCart>
     </CartContainer>
   )
 }
