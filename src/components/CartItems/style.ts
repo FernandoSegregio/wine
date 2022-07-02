6c30df20469b61e85680deeef46146ae4f1695a4
FernandoSegregio <segregio@gmail.com>
2022-07-02 00:46:53 -0300

    style: add transition and styles in CartContainer

--- src/components/CartItems/style.ts
+++ src/components/CartItems/style.ts
@@ -2,7 +2,17 @@ import styled from 'styled-components';
 
 export const CartContainer = styled.div`
   right: 0;
+  z-index: 2;
+  background-color: var(--brand-color-white-150);
   height: 100vw;
   position: fixed;
+  transition-property: all;
+  -webkit-transition-property: all;
+  transition-duration: 0.5s;
+  -webkit-transition-duration: 0.5s;
+  transition-timing-function: ease;
+  -webkit-transition-timing-function: ease;
+  transition-delay: 0s;
+  -webkit-transition-delay: 0s;
   width: 350px;
 `;
