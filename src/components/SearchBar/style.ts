107f3f9eea72bae03120b49398916d4db3b9c5bc
FernandoSegregio <segregio@gmail.com>
2022-07-02 14:23:52 -0300

    style: create Search container, and add styles

---
+++ src/components/SearchBar/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SearchContainer = styled.div`
+  background-color: var(--brand-color-white-150);
+  box-shadow: 0 2px 20px 0 #00000026;
+  height: 73px;
+  position: absolute;
+  top: 88px;
+  width: 100%;
+  z-index: 2;
+`;
+
+export default SearchContainer;
