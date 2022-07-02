13a1c69357498e5d48920ab2bf2d33c50373b028
FernandoSegregio <segregio@gmail.com>
2022-07-02 14:07:32 -0300

    style: change color input radio and create div input

--- src/components/Aside/style.ts
+++ src/components/Aside/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const AsideContainer = styled.aside`
+export const AsideContainer = styled.aside`
   h1 {
     color: var(--brand-color-black-200);
     font-family: Lato;
@@ -10,10 +10,6 @@ const AsideContainer = styled.aside`
     margin-bottom: 32px;
   }
 
-  input {
-    height: 16px;
-    width: 15.89px;
-  }
 
   p {
     color: var(--brand-color-gray-300);
@@ -24,16 +20,74 @@ const AsideContainer = styled.aside`
     margin-bottom: 16px;
   }
 
-  label {
-    align-items: center;
-    display: flex;
-    color: var(--brand-color-gray-300);
-    font-family: Lato;
-    font-size: var(--font-size-14);
-    font-weight: var(--font-weight-normal);
-    line-height: 42px;
-    column-gap: 13.72px;
-  }
 `;
 
-export default AsideContainer;
+export const BorderInput = styled.label`
+  display: flex;
+  position: relative;
+
+      span {
+      color: var(--brand-color-gray-300);
+      font-family: Lato;
+      font-size: var(--font-size-14);
+      font-weight: var(--font-weight-normal);
+      line-height: 42px;   
+      margin-left: 31.6px;  
+
+        &:hover {
+          color: var(--brand-color-pink-100);
+        }
+    }
+
+    label {
+      background: transparent;
+      border-radius: 50%;
+      border: 2px solid var(--brand-color-gray-050);
+      
+      cursor: pointer;
+      
+      height: 16px;
+      left: 0;
+      
+      position: absolute;
+      top: 13px;
+      width: 16px;
+   
+      &:hover::after {
+        background-color: var(--brand-color-pink-050);
+        border: 2px solid white;
+        border-radius: 50%;
+        content: "";
+        height: 6px;
+        left: 1px;
+        position: absolute;
+        top: 1px;
+        width: 6px;
+      }
+
+    }
+
+    input[type="radio"] {
+      visibility: hidden;
+      height: 16px;
+      width: 16px;
+      top: 12px;
+      left: 0;
+      position: absolute;
+    
+      &:checked {
+        visibility: visible;
+        accent-color: var(--brand-color-pink-100);
+        background: var(--brand-color-pink-100);
+        border: 2px solid var(--brand-color-pink-100);
+
+          span {
+            color: var(--brand-color-pink-100);
+          }
+      }
+    } 
+`;
+
+export const Label = styled.label`
+
+`;
