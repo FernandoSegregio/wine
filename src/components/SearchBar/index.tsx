c2253e35105a12eb6d8cad6303b49498a8e0949a
FernandoSegregio <segregio@gmail.com>
2022-07-03 18:34:37 -0300

    feat: create logic search wine filtered

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,16 +1,44 @@
-import React, { useContext } from 'react'
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
 import { VscSearch } from 'react-icons/vsc'
 import { WineContext } from '../../context/wine'
 import SearchContainer from './style'
 
 function Search() {
-  const { openSearch } = useContext(WineContext)
+  const [wines, setWines] = useState([])
+  const [wineSearch, setWineSearch] = useState('')
+  const { openSearch, setWinesFiltered } = useContext(WineContext)
+
+  async function getWinesApi() {
+    const { data } = await axios.get('https://wine-back-test.herokuapp.com/products');
+    setWines(data.items);
+  }
+
+  useEffect(() => {
+    getWinesApi();
+  }, [])
+
+  function handleInput(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const filterWines = wines
+      .filter(({ name }) => name.normalize('NFD' as string).replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '').toLowerCase().includes(wineSearch));
+    setWinesFiltered(filterWines);
+  }
 
   return (
     <div hidden={!openSearch}>
       <SearchContainer>
-        <input type="text" placeholder="Pesquisar" />
-        <VscSearch />
+        <form onSubmit={(e) => handleInput(e)}>
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            onChange={({ target }) => setWineSearch(target.value)}
+            value={wineSearch}
+            name="wineSearch"
+          />
+          <VscSearch />
+        </form>
       </SearchContainer>
     </div>
   )
