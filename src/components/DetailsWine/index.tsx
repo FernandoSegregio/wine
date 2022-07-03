02aa07e3fa0823dce2e31d39f4340cbcd719dc5c
FernandoSegregio <segregio@gmail.com>
2022-07-03 01:21:36 -0300

    feat: create component DetailsWine

---
+++ src/components/DetailsWine/index.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import React, { useContext, useEffect } from 'react'
+import { WineContext } from '../../context/wine';
+
+function DetailsWine() {
+  const { wines } = useContext(WineContext)
+  const { asPath } = useRouter();
+  const idActual = asPath.split('vinho-')[1];
+
+  const wine = wines.find((item) => item.id === Number(idActual));
+
+  useEffect(() => {
+    console.log('sou eu', wine)
+  }, [])
+  return (
+    <div>Details</div>
+  )
+}
+
+export default DetailsWine;
