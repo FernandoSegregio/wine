2e156dd40cd326d9389680d642bc0b0da17d75e7
FernandoSegregio <segregio@gmail.com>
2022-07-03 11:48:30 -0300

    feat: render items in component DetailsWine

--- src/components/DetailsWine/index.tsx
+++ src/components/DetailsWine/index.tsx
@@ -2,8 +2,16 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React, { useContext, useEffect } from 'react'
 import { MdOutlineArrowBackIosNew } from 'react-icons/md'
+import { RiArrowRightSLine } from 'react-icons/ri'
 import { WineContext } from '../../context/wine';
-import { DetailsContainer, ImageContainer } from './style';
+import {
+  DetailsContainer,
+  ImageContainer,
+  OriginAndTypeContainer,
+  PriceContainer,
+  SommelierContainer,
+  Details,
+} from './style';
 
 function DetailsWine() {
   const { wines } = useContext(WineContext)
@@ -26,6 +34,44 @@ function DetailsWine() {
           <Image src={wine.image} alt={wine.name} width={381} height={579} />
         </ImageContainer>
       </div>
+      <Details>
+        <OriginAndTypeContainer>
+          <div>
+            <p>Vinhos</p>
+            <RiArrowRightSLine />
+            <p>{wine.country}</p>
+            <RiArrowRightSLine />
+            <span>{wine.region}</span>
+          </div>
+          <h1>{wine.name}</h1>
+          <div>
+            <Image src={wine.flag} alt={`Flag ${wine.country}`} width={16} height={16} />
+            <p>{wine.country}</p>
+            <p>{wine.type}</p>
+            <p>{wine.classification}</p>
+            <p>{wine.size || wine.volume}</p>
+            <p>{wine.rating}</p>
+          </div>
+        </OriginAndTypeContainer>
+        <PriceContainer>
+          <h1>
+            <span>R$</span>
+            {(wine.priceMember).toString().split('.')[0]}
+            ,
+            <span>{(wine.priceMember).toFixed(2).toString().split('.')[1]}</span>
+          </h1>
+          <p>
+            NÃO SÓCIO R$
+            {' '}
+            {(wine.priceNonMember).toFixed(2).replace('.', ',')}
+          </p>
+        </PriceContainer>
+        <SommelierContainer>
+          <h3>Comentário do Sommelier</h3>
+          <p>{wine.sommelierComment}</p>
+        </SommelierContainer>
+        <button type="button">Adicionar</button>
+      </Details>
     </DetailsContainer>
   )
 }
