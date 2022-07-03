bfad4ad0e4eb70d763d5eeba4988c15e940f621e
FernandoSegregio <segregio@gmail.com>
2022-07-03 03:03:39 -0300

    style: create component DetailsContainer and ImageContainer, add styles

---
+++ src/components/DetailsWine/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const DetailsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 670px 1fr;
+  height: 731px;
+`;
+
+export const ImageContainer = styled.div`
+  height: 579px;
+  margin-left: 41%;
+  margin-top: 50px;
+  width: 381px;
+`;
