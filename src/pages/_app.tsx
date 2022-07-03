28ad2d8eaea74d10d6565f02e497c88f909e1e71
FernandoSegregio <segregio@gmail.com>
2022-07-02 22:42:43 -0300

    feat: pass provider in MyApp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { WineProvider } from '../context/wine';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WineProvider>
+      <Component {...pageProps} />
+    </WineProvider>
+  );
 }
 
 export default MyApp;
