a2bf26cde8aca608a8391651b19ac7ee1cf19973
FernandoSegregio <segregio@gmail.com>
2022-07-03 01:13:40 -0300

    feat: add logic for route details wine, and reanem wines for winesApi

--- src/components/CardWine/index.tsx
+++ src/components/CardWine/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import Router from 'next/router';
 import {
   ImageStyle,
   CardContainer,
@@ -16,54 +17,63 @@ import {
 import { WineProps } from '../../interfaces';
 import blackIcon from '../../../public/images/card/black-wine.svg';
 
-function Card({ wines }: WineProps) {
+function Card({ winesApi }: WineProps) {
+  function nextDetailsWine(id: number) {
+    Router.push({ pathname: `/loja-vinho-${id}` })
+  }
+
   return (
     <>
-      { wines.map((item) => (
+      { winesApi.map((item) => (
         <CardContainer key={item.id}>
-          <section>
-            <ImageStyle
-              src={item.image}
-              alt="teste"
-              height={178}
-              width={119}
-            />
-            <Name>{item.name}</Name>
-            <Image
-              src={blackIcon}
-              alt="Logo Black Wine"
-              width={38.94}
-              height={38.94}
-            />
-            <DiscountContainer>
-              <ValueDiscount>
-                R$
+          <button
+            type="button"
+            onClick={() => nextDetailsWine(item.id)}
+          >
+            <section>
+              <ImageStyle
+                src={item.image}
+                alt="teste"
+                height={178}
+                width={119}
+              />
+              <Name>{item.name}</Name>
+              <Image
+                src={blackIcon}
+                alt="Logo Black Wine"
+                width={38.94}
+                height={38.94}
+              />
+              <DiscountContainer>
+                <ValueDiscount>
+                  R$
+                  {' '}
+                  {(item.price - item.priceMember).toFixed(2).replace('.', ',')}
+                </ValueDiscount>
+                <PercentDiscount>
+                  {item.discount}
+                  %
+                  {' '}
+                  OFF
+                </PercentDiscount>
+              </DiscountContainer>
+              <MemberPriceContainer>
+                <Member>SÓCIO WINE</Member>
                 {' '}
-                {(item.price - item.priceMember).toFixed(2).replace('.', ',')}
-              </ValueDiscount>
-              <PercentDiscount>
-                {item.discount}
-                %
+                <Price>
+                  <span>R$</span>
+                  {(item.priceMember).toString().split('.')[0]}
+                  ,
+                  <span>{(item.priceMember).toFixed(2).toString().split('.')[1]}</span>
+                </Price>
+              </MemberPriceContainer>
+              <NonMember>
+                NÃO SÓCIO R$
                 {' '}
-                OFF
-              </PercentDiscount>
-            </DiscountContainer>
-            <MemberPriceContainer>
-              <Member>SÓCIO WINE</Member>
-              {' '}
-              <Price>
-                <span>R$</span>
-                {(item.priceMember).toString().split('.')[0]}
-                ,
-                <span>{(item.priceMember).toFixed(2).toString().split('.')[1]}</span>
-              </Price>
-            </MemberPriceContainer>
-            <NonMember>
-              NÃO SÓCIO R$
-              {' '}
-              {(item.priceNonMember).toFixed(2).replace('.', ',')}
-            </NonMember>
-          </section>
+                {(item.priceNonMember).toFixed(2).replace('.', ',')}
+              </NonMember>
+            </section>
+          </button>
           <ButtonAdd>ADICIONAR</ButtonAdd>
         </CardContainer>
       ))}
