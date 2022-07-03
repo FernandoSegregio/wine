b2c78df437f620fbd1a2d2e42c7d391ff7ecce1a
FernandoSegregio <segregio@gmail.com>
2022-07-03 18:34:08 -0300

    feat: add state global winesFiltered

--- src/context/wine.tsx
+++ src/context/wine.tsx
@@ -1,12 +1,15 @@
 import React, {
   createContext, Dispatch, SetStateAction, useMemo, useState,
 } from 'react';
+import { ApiItems } from '../interfaces';
 
 interface InterWineContext {
+  winesFiltered: ApiItems[];
   openCart: boolean;
   openSearch: boolean;
   setOpenCart: Dispatch<SetStateAction<boolean>>;
   setOpenSearch: Dispatch<SetStateAction<boolean>>;
+  setWinesFiltered: Dispatch<SetStateAction<ApiItems[]>>
 }
 
 interface InterWineProvider {
@@ -18,14 +21,16 @@ export const WineContext = createContext<InterWineContext>({} as InterWineContex
 export function WineProvider({ children }: InterWineProvider) {
   const [openSearch, setOpenSearch] = useState(false);
   const [openCart, setOpenCart] = useState(false);
+  const [winesFiltered, setWinesFiltered] = useState([] as ApiItems[]);
 
   const context = useMemo(() => ({
-
+    winesFiltered,
     openCart,
     openSearch,
     setOpenCart,
     setOpenSearch,
-  }), [openCart, openSearch]);
+    setWinesFiltered,
+  }), [openCart, openSearch, winesFiltered]);
 
   return (
     <WineContext.Provider value={context}>
