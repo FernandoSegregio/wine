1d2945e8835f5f07968e3a59e1bbf63448c29d3f
FernandoSegregio <segregio@gmail.com>
2022-07-03 03:05:56 -0300

    style: set positin in arrow and image wine

--- src/components/DetailsWine/style.ts
+++ src/components/DetailsWine/style.ts
@@ -4,6 +4,30 @@ export const DetailsContainer = styled.div`
   display: grid;
   grid-template-columns: 670px 1fr;
   height: 731px;
+
+    & > div {
+      margin: 30.77px auto;
+      width: 100%;
+        
+        & > span {
+        display: flex;
+        margin-left: 19%;
+
+          & > p {
+            margin-left: 16px;
+            font-family: Neo Sans;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 20px;
+            line-height: 24px;
+            color: #111111;
+          }
+
+          & > svg {
+            font-size: 22px;
+          } 
+      }
+    }
 `;
 
 export const ImageContainer = styled.div`
