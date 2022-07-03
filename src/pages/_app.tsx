27edcc989af1e802e748fcc28ba918799b11cbdd
FernandoSegregio <segregio@gmail.com>
2022-07-03 01:18:07 -0300

    refactor: pass GlobalStyle for MyApp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { WineProvider } from '../context/wine';
+import GlobalStyle from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WineProvider>
+      <GlobalStyle />
       <Component {...pageProps} />
     </WineProvider>
   );
