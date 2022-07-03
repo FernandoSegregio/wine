8ed75a43fb69292cbe759a04c3e9b84c93af7a46
FernandoSegregio <segregio@gmail.com>
2022-07-02 22:47:05 -0300

    feat: add logic openSearch in search

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { VscSearch } from 'react-icons/vsc'
+import { WineContext } from '../../context/wine'
 import SearchContainer from './style'
 
 function Search() {
-  return (
-    <SearchContainer>
-      <input type="text" placeholder="Pesquisar" />
-      <VscSearch />
+  const { openSearch } = useContext(WineContext)
 
-    </SearchContainer>
+  return (
+    <div hidden={!openSearch}>
+      <SearchContainer>
+        <input type="text" placeholder="Pesquisar" />
+        <VscSearch />
+      </SearchContainer>
+    </div>
   )
 }
 
