0ed2952fe34d260703db17f4c0e5cbf88d68f500
FernandoSegregio <segregio@gmail.com>
2022-07-03 21:56:13 -0300

    feat: add function helpers localStorage

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,19 @@
 import cookie from 'cookie'
 import { IncomingMessage } from 'http'
+import { Dispatch, SetStateAction } from 'react';
+import { ApiItems } from '../interfaces';
 
-export default function parseCookies(req: IncomingMessage | undefined) {
+export function parseCookies(req: IncomingMessage | undefined) {
   return cookie.parse(req ? req.headers.cookie || '' : document.cookie)
+}
+
+export function addLocalStorage(cartList: ApiItems[]) {
+  localStorage.setItem('cart', JSON.stringify(cartList));
+}
+
+export function restoreCartList(setCartList: Dispatch<SetStateAction<ApiItems[]>>) {
+  const actualCart = JSON.parse(localStorage.getItem('cart') as string);
+  if (actualCart !== null) {
+    setCartList(actualCart);
+  }
 }
