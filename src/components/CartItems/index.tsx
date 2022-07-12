8123727ae5180ed029e7afec1f2989ab446d0bb7
FernandoSegregio <segregio@gmail.com>
2022-07-12 01:14:22 -0300

    feat: add logic background opacity in main

--- src/components/CartItems/index.tsx
+++ src/components/CartItems/index.tsx
@@ -2,49 +2,60 @@ import React, { useContext } from 'react'
 import { BsArrowLeft } from 'react-icons/bs'
 import { WineContext } from '../../context/wine';
 import CardCart from '../CardCart';
-import { CartContainer, FooterCart, HeaderCart } from './style';
+import {
+  CartContainer, FooterCart, HeaderCart, CloseCart,
+} from './style';
 
 function Cart() {
-  const { openCart, setOpenCart, cartList } = useContext(WineContext);
+  const {
+    openCart, setOpenCart, cartList,
+  } = useContext(WineContext);
 
   return (
-    <CartContainer hidden={!openCart}>
-      <HeaderCart>
-        <button type="button" onClick={() => setOpenCart(false)}>
-          <BsArrowLeft />
-        </button>
-        <p>WineBox</p>
-        <span>
-          (
-          {cartList.length > 0 ? cartList
-            .map((item) => item.quantity).reduce((a, b) => a + b) : 0 }
-          )
-        </span>
-      </HeaderCart>
-      <section>
-        { cartList.length === 0 ? (
-          <>
-            <p>
-              =(
-            </p>
-            <p>Você ainda não escolheu seus produtos</p>
-          </>
-        ) : <CardCart />}
-      </section>
-      <FooterCart>
-        <div>
-          <h3>Total</h3>
-          <h1>
-            R$
-            {' '}
-            {cartList.length > 0 ? Number(cartList
-              .map((item) => item.quantity * item.priceMember)
-              .reduce((a, b) => a + b)).toFixed(2) : 0 }
-          </h1>
-        </div>
-        <button type="button">Finalizar pedido</button>
-      </FooterCart>
-    </CartContainer>
+    <>
+      <CloseCart onClick={() => setOpenCart(false)} type="button" hidden={!openCart} />
+      <CartContainer hidden={!openCart}>
+        <HeaderCart>
+          <button
+            type="button"
+            onClick={() => setOpenCart(false)}
+          >
+            <BsArrowLeft />
+          </button>
+          <p>WineBox</p>
+          <span>
+            (
+            {cartList.length > 0 ? cartList
+              .map((item) => item.quantity).reduce((a, b) => a + b) : 0}
+            )
+          </span>
+        </HeaderCart>
+        <section>
+          {cartList.length === 0 ? (
+            <>
+              <p>
+                =(
+              </p>
+              <p>Você ainda não escolheu seus produtos</p>
+            </>
+          ) : <CardCart />}
+        </section>
+        <FooterCart>
+          <div>
+            <h3>Total</h3>
+            <h1>
+              R$
+              {' '}
+              {cartList.length > 0 ? Number(cartList
+                .map((item) => item.quantity * item.priceMember)
+                .reduce((a, b) => a + b)).toFixed(2) : 0}
+            </h1>
+          </div>
+          <button type="button">Finalizar pedido</button>
+        </FooterCart>
+      </CartContainer>
+
+    </>
   )
 }
 
